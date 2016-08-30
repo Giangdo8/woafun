ec91a5191cb5733dc5415fc82092199e5f057580
hixbotay <vuonganhduong812@gmail.com>
2016-08-30 22:34:31 +0700

    test

--- wp-content/themes/seopress/template-parts/header-slider.php
+++
@@ -1,9 +0,0 @@
-<?php
-if( get_the_ID() )
-{
-	if( class_exists( 'RWMB_Loader' ) && rwmb_meta( 'seopress_slider_scode', '', $post->ID ) )
-	{
-		echo do_shortcode( rwmb_meta( 'seopress_slider_scode', '', $post->ID ) );
-	}
-}
-?>
